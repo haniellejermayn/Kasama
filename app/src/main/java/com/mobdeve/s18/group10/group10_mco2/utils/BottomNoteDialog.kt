95f11b8a7e6b76e0b45bfedcb72ba22296a029a6
zi <161949268+strangeshands@users.noreply.github.com>
2025-10-18 15:46:34 +0800

    Note dialog layout fixed

--- app/src/main/java/com/mobdeve/s18/group10/group10_mco2/utils/BottomNoteDialog.kt
+++ app/src/main/java/com/mobdeve/s18/group10/group10_mco2/utils/BottomNoteDialog.kt
@@ -1,11 +1,13 @@
 package com.mobdeve.s18.group10.group10_mco2.utils
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.widget.Toast
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.mobdeve.s18.group10.group10_mco2.Note
 import com.mobdeve.s18.group10.group10_mco2.databinding.LayoutBottomNoteDetailBinding
+import android.view.View
 
 fun showNoteBottomSheet(
     context: Context,
@@ -13,6 +15,7 @@ fun showNoteBottomSheet(
     onSave: (title: String, content: String) -> Unit
 ) {
     val bottomSheet = BottomSheetDialog(context)
+    bottomSheet.behavior.isFitToContents = true
     val binding = LayoutBottomNoteDetailBinding.inflate(LayoutInflater.from(context))
 
     binding.editTextTitle.setText(note?.title ?: "")
@@ -32,6 +35,14 @@ fun showNoteBottomSheet(
         }
     }
 
+    bottomSheet.window?.setBackgroundDrawableResource(android.R.color.transparent)
     bottomSheet.setContentView(binding.root)
+
+    bottomSheet.setOnShowListener {
+        val bottomSheetView =
+            bottomSheet.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+        bottomSheetView?.setBackgroundColor(Color.TRANSPARENT)
+    }
+
     bottomSheet.show()
 }
